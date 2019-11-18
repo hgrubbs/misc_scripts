954b54a953f349486b12cbdd3f68e1820df4fdfe
hgrubbs <hunter.grubbs@protonmail.com>
2019-11-18 05:45:53 +0000

    new setup

--- setup_vim_tmux_bashrc.sh
+++ setup_vim_tmux_bashrc.sh
@@ -7,4 +7,6 @@ echo "adding tmux.conf"
 cp tmux/tmux.conf ~/.tmux.conf
 
 echo "adding bashrc template to .bashrc"
+echo '# ---BEGIN misc_scripts bashrc---' >> ~/.bashrc
 cat bash/bashrc >> ~/.bashrc
+echo "# ---END misc_scripts bashrc---" >> ~/.bashrc
