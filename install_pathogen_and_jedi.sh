3846737398b4d325b67eccc6e862815b0fc5f354
hgrubbs <hunter.grubbs@protonmail.com>
2019-11-18 05:43:54 +0000

    added pathogen/jedi install script

---
+++ install_pathogen_and_jedi.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -x
+
+echo "installing pathogen"
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+echo "installing jedi-vim"
+git clone --recursive https://github.com/davidhalter/jedi-vim.git ~/.vim/bundle/jedi-vim
+
+echo "updating jedi-vim"
+PWD=$(pwd)
+cd ~/.vim/bundle/jedi-vim
+git clone --recursive https://github.com/davidhalter/jedi-vim.git ~/.vim/bundle/jedi-vim
+cd ${PWD}
