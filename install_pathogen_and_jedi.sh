6e4af9c1db8a8d7251b99645a1e0eba824c030eb
hgrubbs <hunter.grubbs@protonmail.com>
2020-01-29 05:28:10 +0000

    Revert "experimental"
    This reverts commit ebdee772b5366b5b89bd2baa422c83845a778b9e.

--- install_pathogen_and_jedi.sh
+++ install_pathogen_and_jedi.sh
@@ -1,8 +1,19 @@
 #!/bin/sh -x
 
-mkdir -p ~/.vim/bundle
-echo "installing vundle"
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo "installing pathogen"
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
+echo "installing jedi-vim"
+git clone --recursive https://github.com/davidhalter/jedi-vim.git ~/.vim/bundle/jedi-vim
+
+echo "updating jedi-vim"
 PWD=$(pwd)
+cd ~/.vim/bundle/jedi-vim
+git submodule update --init --recursive
+cd ${PWD}
+
+echo "installing logstash.vim"
+cd ~.vim/bundle
+git clone https://github.com/prettier/vim-prettier
 cd ${PWD}
