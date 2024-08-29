17911773cc0293a5aa7d87c0ea95fe1201be75e5
hgrubbs <hunter.grubbs@protonmail.com>
2024-08-29 09:06:53 -0500

    remove outdated/obsolete assets

--- ubuntu/deploy_ubuntu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir -p ~/local/{bin,log}
-cp ../vim/vimrc ~/.vimrc
-cp ../tmux/tmux.conf ~/.tmux.conf
-cp ubuntu_tweaks.sh ~/local/bin/
-cat ../bash/bashrc >> ~/.bashrc
-cat bashrc >> ~/.bashrc
--- ubuntu/ubuntu_tweaks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sleep 2
-
-sudo powertop --auto-tune
-touch $HOME/local/log/powertop_tuned
-
-killall syndaemon
-syndaemon -i 0.50 -m 0.050 -K -v -d -R &> /dev/null
-touch $HOME/local/log/syndaemon_restarted
