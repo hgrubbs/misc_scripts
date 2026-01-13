df06178e35fe9b452dcd705850b253c0f71808ea
hgrubbs <hunter.grubbs@protonmail.com>
2026-01-13 08:11:41 -0600

    add simple borg shellscript

---
+++ tools/borg_backup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+set -x
+set -e
+shopt -s expand_aliases
+
+
+touch /media/$USER/borg/this_is_a_test_file
+
+alias timestamp_military='date +"%Y%m%d.%H%M%S"'
+borg create --compression lz4 --stats --progress /media/hgrubbs/borg/borg_repo_1::$(timestamp_military) $HOME
